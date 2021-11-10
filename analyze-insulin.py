099b4c56dd9213e5a3d95eb6ad055a2901382398
Alumasa <alumasa.v@gmail.com>
2021-11-10 21:16:05 +0300

    Analyzing Insulin with Python

---
+++ analyze-insulin.py
@@ -0,0 +1,20 @@
+"""
+Your module description
+"""
+print(len("malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"))
+insulin = "malwmrllpllallalwgpdpaaafvnqhlcgshlvealylvcgergffytpktrreaedlqvgqvelgggpgagslqplalegslqkrgiveqcctsicslyqlenycn"
+new_insulin=insulin[0:24]
+print(len(new_insulin))
+print(new_insulin)
+
+x_insulin = insulin[24:54]
+print(len(x_insulin))
+print(x_insulin)
+
+y_insulin = insulin[54:89]
+print(len(y_insulin))
+print(y_insulin)
+
+z_insulin = insulin[89:]
+print(len(z_insulin))
+print(z_insulin)
